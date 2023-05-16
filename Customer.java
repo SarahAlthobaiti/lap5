c51cb0423cc7e96cbeda729022589bddeb9d54cc
SarahAlthobaiti <120081509+SarahAlthobaiti@users.noreply.github.com>
2023-05-16 10:00:53 +0000

    co3

--- Customer.java
+++ Customer.java
@@ -1,56 +1,68 @@
 public class Customer {
-    private String name;
-    private String contactNumber;
-    private String email;
-    private Order order ;
+    private String name; // customer name
+    private String contactNumber; // customer contact number
+    private String email; // customer email address
+    private Order order; // customer order
     
-
+    // default constructor
     public Customer(){
-
+        
     }
-
-
-    public Customer(String name, String contactNumber, String emai,Order order) {
+    
+    // parameterized constructor
+    public Customer(String name, String contactNumber, String email, Order order) {
         this.name = name;
         this.contactNumber = contactNumber;
         this.email = email;
+        // creates a copy of the provided order object and sets it as the customer order
         this.order = order.copy();
     }
     
+    // setter for customer name
     public void setName(String name) {
         this.name = name;
     }
 
+    // setter for customer contact number
     public void setContactNumber(String contactNumber) {
         this.contactNumber = contactNumber;
     }
     
+    // setter for customer email address
     public void setEmail(String email) {
         this.email = email;
     }
 
-    public void setOrder(Order ord){
-        this.order = ord.coby();
+    // setter for customer order
+    public void setOrder(Order order){
+        // creates a copy of the provided order object and sets it as the customer order do not return the private obj
+        this.order = order.copy();
     }
 
+    // getter for customer name
     public String getName() {
         return name;
     }
     
+    // getter for customer contact number
     public String getContactNumber() {
         return contactNumber;
     }
     
+    // getter for customer email address
     public String getEmail() {
         return email;
     }
 
+    // getter for customer order
     public String getOrder(){
-        return Order.coby();
+        // returns a string representation of the customer order
+        return order.toString();
     }
     
-    
+    //Information about the customer.
     public String toString() {
-        return "Name: " + name + "\nContact number: " + contactNumber + "\nEmail: " + email + "\nOrder Informaition: " + Order.toString(); ;
+        // returns a string containing customer name, contact number, email address, and order information
+        return "Name: " + name + "\nContact number: " + contactNumber + "\nEmail: " + email + "\nOrder Information: " + order.toString();
     }
 }
