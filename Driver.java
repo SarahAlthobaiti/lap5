f9617e9f9f4d7d78f8f656705adb3c95bd735eb5
SarahAlthobaiti <120081509+SarahAlthobaiti@users.noreply.github.com>
2023-05-24 12:18:15 +0000

    sdfg

--- Driver.java
+++ Driver.java
@@ -1,10 +1,9 @@
-import java.util.ArrayList;
-import java.util.Scanner;
-import java.io.FileNotFoundException;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.PrintWriter;
-import java.io.IOException;
+import java.util.ArrayList; // importing the ArrayList class
+import java.util.Scanner; // importing the Scanner class
+import java.io.FileNotFoundException; // importing the FileNotFoundException
+import java.io.File; // importing the File class
+import java.io.PrintWriter; // Importing the printWriter class
+import java.io.IOException; // importing the IOException
 /**
  *
  * @author 
@@ -15,43 +14,55 @@ public class Driver {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        // Creating an object from the class Scanner
         Scanner userIn = new Scanner(System.in);
+        // Printing a Welcome massage to the user
         System.out.println("\"Welcome to our CAKE BARKERY\"");
+        // Creating an A arraList to hold information about the Cake 
         ArrayList<Cake> cakesInfo = new ArrayList<Cake>();
-        
+        // Creating a file variabal of type String store the file name
         String file ="text.txt";
+        // declaer scanner object and initialized it to null
         Scanner scanner =null;
         
       try{  
+        // Creating new Scanner object  to read from the file
         scanner = new Scanner(new File(file));
+        // Read the file lins one by one
         while(scanner.hasNextLine()){
+            // Reading each line of the file and splitting it
+            // into parts based on the comma separator
             String line=scanner.nextLine();
             String[]parts=line.split(",");
-            
+            // Extracet each item into an appropriate variable
             String name = parts[0];
             int id =Integer.parseInt(parts[1].trim());
             double calories = Double.parseDouble(parts[2].trim());
             double cost = Double.parseDouble(parts[3].trim());
             String flavor =parts[4];
             int size= Integer.parseInt(parts[5].trim());
-            
+            // Creating new object from the Cake class 
             Cake cake =new Cake(name,id,calories,cost,flavor,size);
-            
+            // add the object to the arrayList
             cakesInfo.add(cake);
             
         }
-       
+       // close the Scanner after reading from the file
         scanner.close();
       }
-      
+      // Catching an exception if the file is not found
       catch(FileNotFoundException e){
           System.out.println(e.getMessage());
       }
-
+      // Printing the list of available cakes to the user
         System.out.println("Here is our meneu: "+"\n");
         System.out.println(cakesInfo);
+        // Prompt the user to choose the Cake by the Id Cake number
         System.out.println("please choose number of the cake that you want");
         int Id = userIn.nextInt();
+
+        // Checking if the cake ID entered by the user is valid and
+        // converting it to the corresponding index in the cakesInfo ArrayList
         int index =0;
         boolean check = true;
         while (check){
@@ -90,27 +101,37 @@ public class Driver {
                  } 
            check = false;
         }else{
+            // Prompt the user to enter a valid cake ID
             System.out.println("Please try againe invalid number");
             Id = userIn.nextInt();
         }
     }
-        //System.out.println("Do you want to order onother cake? yes/no");
-        
+        // Creating an Order object with the selected cake
         Order order1 = new Order(cakesInfo.get(index));
+
+        // Prompot the user to enter his name
         System.out.println("Enter your name: ");
+        // Consuming the newline character left by the previous user input
         userIn.nextLine();
         String name = userIn.nextLine();
+         // Prompot the user to enter his phone number
         System.out.println("Enter your phone number: ");
         String phoneNum = userIn.nextLine();
+         // Prompot the user to enter his Email
         System.out.println("Enter your email: ");
         String email = userIn.nextLine();
-        
+        // Printing a new line
         System.out.println();
+         // Prompot the user to choose the payment method
         System.out.println("write payment method cash or credit card : ");
         String paymentWay = userIn.nextLine();
+        // declare an object of type payment
         Payment d;
+        // declare an object of type cash
         Cash pay;
+        // declare an object of type creditCard
         CreditCard pay2;
+        
         if(paymentWay.equalsIgnoreCase("Cash")){
             d = new Payment(paymentWay);
             System.out.println("Enter the day of received :");
